21512d1701dc514a1d1c86a74bf509413961f1b8
bururun <bururun@users.noreply.github.com>
2025-10-01 23:32:30 +0300

    ci: performance optimization

--- src/index.js
+++ src/index.js
@@ -21,3 +21,28 @@ class SniffleLogger {
 
 // Update 8
 module.exports = SniffleLogger;
+
+
+// Main entry point for SniffleLogger
+
+class SniffleLogger {
+    constructor() {
+        this.initialized = true;
+        this.version = '1.0.10';
+    }
+    
+    start() {
+        return 'Started';
+    }
+    
+    stop() {
+        return 'Stopped';
+    }
+    
+    getStatus() {
+        return { status: 'running', version: this.version };
+    }
+}
+
+// Update 10
+module.exports = SniffleLogger;
