6fc2b20a65abc806d45ac3e964708e306cd4401d
bururun <bururun@users.noreply.github.com>
2025-10-03 13:14:32 +0300

    ci: update README

--- src/index.js
+++ src/index.js
@@ -121,3 +121,28 @@ class SniffleLogger {
 
 // Update 26
 module.exports = SniffleLogger;
+
+
+// Main entry point for SniffleLogger
+
+class SniffleLogger {
+    constructor() {
+        this.initialized = true;
+        this.version = '1.0.32';
+    }
+    
+    start() {
+        return 'Started';
+    }
+    
+    stop() {
+        return 'Stopped';
+    }
+    
+    getStatus() {
+        return { status: 'running', version: this.version };
+    }
+}
+
+// Update 32
+module.exports = SniffleLogger;
