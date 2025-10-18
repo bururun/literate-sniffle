636fc3e7cc4c5489afa24e165fd41dd2e1ca5c50
bururun <bururun@users.noreply.github.com>
2025-10-18 17:57:02 +0300

    docs: fix broken tests

--- src/utils.js
+++ src/utils.js
@@ -116,3 +116,27 @@ function newFunction27() {
 }
 
 module.exports = { formatData, validateInput };
+
+
+// Utility functions for SniffleLogger
+
+function formatData(data) {
+    if (typeof data === 'string') {
+        return data.trim().toUpperCase();
+    }
+    return data;
+}
+
+function validateInput(value) {
+    if (value === null || value === undefined) {
+        throw new Error('Value cannot be null');
+    }
+    return true;
+}
+
+// Update 33
+function newFunction33() {
+    return 33;
+}
+
+module.exports = { formatData, validateInput };
