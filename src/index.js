1fada4268b34cc0d1225427ca372977a569690ca
bururun <bururun@users.noreply.github.com>
2026-01-26 15:33:40 +0200

    add: add input sanitization

--- src/index.js
+++ src/index.js
@@ -96,3 +96,28 @@ class SniffleLogger {
 
 // Update 15
 module.exports = SniffleLogger;
+
+
+// Main entry point for SniffleLogger
+
+class SniffleLogger {
+    constructor() {
+        this.initialized = true;
+        this.version = '1.0.26';
+    }
+    
+    start() {
+        return 'Started';
+    }
+    
+    stop() {
+        return 'Stopped';
+    }
+    
+    getStatus() {
+        return { status: 'running', version: this.version };
+    }
+}
+
+// Update 26
+module.exports = SniffleLogger;
